bcff0a29fed64da8acd69b1ac52ba184a85bdaaf
Heiko Goes <heiko.goes@gaebler-goes.de>
2024-05-15 07:13:49 +0200

    added default new

--- lib/svg.ex
+++ lib/svg.ex
@@ -9,11 +9,13 @@ defmodule Graphics.SVG do
     %__MODULE__{width: width, height: height, contents: contents}
   end
 
+  def new(), do: __struct__()
+
   def add(%{contents: contents} = svg, element) do
-    %Graphics.SVG{svg | contents: [element | contents]}
+    %__MODULE__{svg | contents: [element | contents]}
   end
 
-  def render(svg = %Graphics.SVG{}) do
+  def render(svg = %__MODULE__{}) do
     """
     <svg width="#{svg.width}" height="#{svg.height}" xmlns="http://www.w3.org/2000/svg">
       #{Enum.join(svg.contents, "\n")}
